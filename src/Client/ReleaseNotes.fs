b7725abc5cca8dd1891993dbe3d64d8978ab130c
Evelina Gabasova <evelina@evelinag.com>
2018-09-11 16:41:34 +0100

    Bumping version to 0.1.5.2

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.1.5.1"
+let Version = "0.1.5.2"
 
 let IsPrerelease = false
 
 let Notes = """
+### 0.1.5.2 - 2018-09-11
+* Debugging
+
 ### 0.1.5.1 - 2018-09-11
 * Debugging
 
