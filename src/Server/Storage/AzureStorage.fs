9280c3cc8a2b704bb3b83e3f13fb78c68d49d0b1
Evelina Gabasova <evelina@evelinag.com>
2018-10-22 14:21:40 +0100

    Fixed annotations db integration

--- src/Server/Storage/AzureStorage.fs
+++ src/Server/Storage/AzureStorage.fs
@@ -15,6 +15,7 @@ open System.Text.RegularExpressions
 open System.Data.SqlClient
 open FSharp.Data
 open System.Text.RegularExpressions
+open System.Data
 
 //type Metadata = JsonProvider<"""{"microdata": [], "json-ld": [], "opengraph": [{"namespace": {"og": "http://ogp.me/ns#", "fb": "http://ogp.me/ns/fb#", "article": "http://ogp.me/ns/article#"}, "properties": [["fb:app_id", "529217710562597"], ["fb:pages", "146422995398181"], ["og:type", "article"], ["og:title", "Yale Alumnae Stand Up To Kavanaugh, Support His Latest Accuser By The Hundreds"], ["og:url", "http://addictinginfo.com/2018/09/24/yale-alumnae-stand-up-to-kavanaugh-support-his-latest-accuser-by-the-hundreds/"], ["og:description", "But of course, the GOP-led Senate seems to be unmoved by any of this. "], ["og:site_name", "AddictingInfo"], ["og:image", "http://addictinginfo.com/wp-content/uploads/2018/09/Screen-Shot-2018-09-23-at-7.52.05-PM.jpg"], ["og:locale", "en_US"], ["article:author", "http://facebook.com/shannonforequality"], ["article:publisher", "https://www.facebook.com/politicsbyjustin/"], ["fb:op-recirculation-ads", "placement_id=470010220032878_503313496702550"]]}], "microformat": [{"type": ["h-entry"], "properties": {"category": ["news"], "name": ["Yale Alumnae Stand Up To Kavanaugh, Support His Latest Accuser By The Hundreds"], "url": ["http://addictinginfo.com/2018/09/24/yale-alumnae-stand-up-to-kavanaugh-support-his-latest-accuser-by-the-hundreds/"], "updated": ["2018-09-24T14:42:34+00:00"], "published": ["2018-09-24T14:42:34+00:00"], "content": [{"html": "<p>Supreme Court nominee Brett Kavanaugh is embroiled in scandals surrounding his alleged past as a sexual assaulter. Aside from Dr. Christine Blasey Ford\u2019s accusation that Kavanaugh violently </div>", "value": "Supreme Court nominee Brett Kavanaugh is embroiled in scandals surrounding his alleged past as a sexual assaulter. Aside from Dr. Christine Blasey Ford\u2019s accusation that Kavanaugh violently sexually assaulted her when they were both in high sTwitter"}]}, "children": [{"type": ["h-card"], "properties": {"photo": ["http://0.gravatar.com/avatar/33eb8e63d6f325bdcf229ce66edc227a?s=70&d=identicon&r=g"], "name": ["Shannon Barber"], "url": ["http://addictinginfo.com/author/shannon-barber/"]}}]}], "rdfa": [{"@id": "http://addictinginfo.com/2018/09/24/yale-alumnae-stand-up-to-kavanaugh-support-his-latest-accuser-by-the-hundreds/", "article:author": [{"@value": "http://facebook.com/shannonforequality"}], "article:publisher": [{"@value": "https://www.facebook.com/politicsbyjustin/"}], "http://ogp.me/ns#description": [{"@value": "But of course, the GOP-led Senate seems to be unmoved by any of this. "}], "http://ogp.me/ns#image": [{"@value": "http://addictinginfo.com/wp-content/uploads/2018/09/Screen-Shot-2018-09-23-at-7.52.05-PM.jpg"}], "http://ogp.me/ns#locale": [{"@value": "en_US"}], "http://ogp.me/ns#site_name": [{"@value": "AddictingInfo"}], "http://ogp.me/ns#title": [{"@value": "Yale Alumnae Stand Up To Kavanaugh, Support His Latest Accuser By The Hundreds"}], "http://ogp.me/ns#type": [{"@value": "article"}], "http://ogp.me/ns#url": [{"@value": "http://addictinginfo.com/2018/09/24/yale-alumnae-stand-up-to-kavanaugh-support-his-latest-accuser-by-the-hundreds/"}], "http://ogp.me/ns/fb#app_id": [{"@value": "529217710562597"}], "http://ogp.me/ns/fb#op-recirculation-ads": [{"@value": "placement_id=470010220032878_503313496702550"}], "http://ogp.me/ns/fb#pages": [{"@value": "146422995398181"}], "https://api.w.org/": [{"@id": "http://addictinginfo.com/wp-json/"}]}]}""">
 
@@ -63,30 +64,37 @@ SELECT * FROM sorted_articles_by_site WHERE row_num <= %i" numArticlesPerSite
 
   use cmd = new SqlCommand(command, conn)
   let rdr = cmd.ExecuteReader()
-  [| while rdr.Read() do 
-        yield { Id = rdr.GetInt32(0)
-                SiteName = rdr.GetString(1)
-                CrawlDate = rdr.GetDateTimeOffset(2)
-                ArticleUrl = rdr.GetString(3) 
-                Content = None
-                Metadata = rdr.GetString(5) |> toUnicode } |]
+  let result = 
+      [| while rdr.Read() do 
+            yield { Id = rdr.GetInt32(0)
+                    SiteName = rdr.GetString(1)
+                    CrawlDate = rdr.GetDateTimeOffset(2)
+                    ArticleUrl = rdr.GetString(3) 
+                    Content = None
+                    Metadata = rdr.GetString(5) |> toUnicode } |]
+  conn.Close()
+  result  
 
-let selectArticle id sqlConn =
+let selectArticle articleId sqlConn =
   use conn = new System.Data.SqlClient.SqlConnection(sqlConn)
   conn.Open()
 
-  let command = sprintf "SELECT * FROM [articles] WHERE [article_url] = '%s';" id
-
+  let command = "SELECT * FROM [articles] WHERE [article_url] = @ArticleId;" 
   use cmd = new SqlCommand(command, conn)
+  cmd.Parameters.AddWithValue("@ArticleId", articleId) |> ignore
+
   let rdr = cmd.ExecuteReader()
-  [| while rdr.Read() do 
-        yield { Id = rdr.GetInt32(0)
-                SiteName = rdr.GetString(1)
-                CrawlDate = rdr.GetDateTimeOffset(2)
-                ArticleUrl = rdr.GetString(3)
-                Content = Some (rdr.GetString(4) |> toUnicode) 
-                Metadata = rdr.GetString(5) |> toUnicode } |]    
-  |> Array.exactlyOne
+  let result =
+      [| while rdr.Read() do 
+            yield { Id = rdr.GetInt32(0)
+                    SiteName = rdr.GetString(1)
+                    CrawlDate = rdr.GetDateTimeOffset(2)
+                    ArticleUrl = rdr.GetString(3)
+                    Content = Some (rdr.GetString(4) |> toUnicode) 
+                    Metadata = rdr.GetString(5) |> toUnicode } |]    
+      |> Array.exactlyOne
+  conn.Close()  
+  result
 
 let getJSONFileName userName (articleId: string) = 
     let id = articleId.Replace("/", "-")
@@ -108,25 +116,17 @@ let getExistingAnnotationBlob ( connectionString : AzureConnection) userName art
 }
 
 let checkAnnotationsExist (connectionString : AzureConnection) userName articles  = task {
-    //let blobClient = (CloudStorageAccount.Parse connectionString.BlobConnection).CreateCloudBlobClient()
-    //let container = blobClient.GetContainerReference("sample-crawl")
-
-    // let annotations = 
-    //     articles 
-    //     |> Array.map (fun article ->
-    //         let annotationBlob = container.GetBlockBlobReference ("annotations/" + (getJSONFileName userName article.ArticleUrl))
-    //         article, annotationBlob.ExistsAsync().Result)
-
-    // return annotations
 
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = sprintf "SELECT * FROM [annotations] WHERE user_id='%s' AND num_sources IS NOT NULL;" userName
+    let command = "SELECT article_url FROM [annotations] WHERE user_id = @UserId AND num_sources IS NOT NULL;"
     use cmd = new SqlCommand(command, conn)
+    cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
+
     let rdr = cmd.ExecuteReader()
     let result = 
         [| while rdr.Read() do 
-            let article_url = rdr.GetString(1)
+            let article_url = rdr.GetString(0)
             yield article_url |]
         |> set
     let annotations =
@@ -134,6 +134,7 @@ let checkAnnotationsExist (connectionString : AzureConnection) userName articles
         |> Array.map (fun article ->  
             article, 
             if result.Contains(article.ArticleUrl) then true else false)
+    conn.Close()
     return annotations        
 }
     
@@ -273,22 +274,24 @@ let saveAnnotationsToDB connectionString (annotations: ArticleAnnotations) = tas
 
     // TODO: Check if there already is an entry with the same user-article pair
     // and if so, then overwrite that
-    let command = 
-      sprintf "
-        INSERT INTO [annotations](article_url, annotation, user_id, user_proficiency,created_date, updated_date, num_sources) 
-        VALUES ('%s','%s','%s','%s','%s','%s', %d);"  
-        annotations.ArticleID 
-        (toJson annotations) 
-        annotations.User.UserName 
-        (string annotations.User.Proficiency)
-        (annotations.CreatedUTC.Value |> string) 
-        (System.DateTime.UtcNow |> string )
-        annotations.Annotations.Length
-    let cmd = SqlCommand(command, conn)
-    let! result = cmd.ExecuteNonQueryAsync()
+    let commandText = 
+       "INSERT INTO [annotations](article_url, annotation, user_id, user_proficiency,created_date, updated_date, num_sources) 
+        VALUES (@Url, @Annotation, @UserId, @Proficiency, @CreatedDate, @UpdatedDate, @NumSources);"
+    let cmd = new SqlCommand(commandText, conn)
+    
+    cmd.Parameters.AddWithValue("@Url", annotations.ArticleID) |> ignore
+    cmd.Parameters.AddWithValue("@Annotation", toJson annotations)  |> ignore
+    cmd.Parameters.AddWithValue("@UserId", annotations.User.UserName)  |> ignore
+    cmd.Parameters.AddWithValue("@Proficiency", string annotations.User.Proficiency)  |> ignore
+    cmd.Parameters.AddWithValue("@CreatedDate", annotations.CreatedUTC.Value)  |> ignore
+    cmd.Parameters.AddWithValue("@UpdatedDate", System.DateTime.UtcNow)  |> ignore
+    cmd.Parameters.AddWithValue("@NumSources", annotations.Annotations.Length)  |> ignore
 
+    let result = cmd.ExecuteNonQuery()
+    printfn "DB insert: %A" result
 //    let! annotationBlob = getAnnotationsBlob connectionString annotations.User.UserName annotations.ArticleID
 //    do! annotationBlob.UploadTextAsync(FableJson.toJson annotations)
+    conn.Close()
     return ()
 }
 
@@ -300,12 +303,13 @@ let deleteAnnotationsFromDB connectionString (annotations: ArticleAnnotations) =
 
     let user = annotations.User.UserName
     let id = annotations.ArticleID
-    let command = 
-      sprintf "
-        DELETE FROM [annotations] WHERE article_url = '%s' AND user_id = '%s';"  
-        annotations.ArticleID annotations.User.UserName 
-    let cmd = SqlCommand(command, conn)
-    let! result = cmd.ExecuteNonQueryAsync()
+    let command = "DELETE FROM [annotations] WHERE article_url = @ArticleUrl AND user_id = @UserId;"  
+    let cmd = new SqlCommand(command, conn)
+    cmd.Parameters.AddWithValue("@ArticleUrl", annotations.ArticleID) |> ignore
+    cmd.Parameters.AddWithValue("@UserId", annotations.User.UserName) |> ignore
+
+    let result = cmd.ExecuteNonQuery()
+    conn.Close()
     if result = 1 then return true else return false
 }
 
@@ -326,15 +330,19 @@ let loadArticleAnnotationsFromDB connectionString articleId userName  = task {
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
 
-    let command = sprintf "SELECT * FROM [annotations] WHERE [article_url]='%s' AND user_id='%s';" articleId userName
-
+    let command = "SELECT annotation FROM [annotations] WHERE article_url = @ArticleUrl AND user_id = @UserId;" 
     use cmd = new SqlCommand(command, conn)
+    cmd.Parameters.AddWithValue("@ArticleUrl", articleId) |> ignore
+    cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
+
     let rdr = cmd.ExecuteReader()
     let result = 
         [| while rdr.Read() do 
-            let text = rdr.GetString(2)
+            let text = rdr.GetString(0)
             let ann = text |> FableJson.ofJson<ArticleAnnotations>
             yield Some ann |]
+    conn.Close()
+
     return (
         match result.Length with
         | 0 -> None
