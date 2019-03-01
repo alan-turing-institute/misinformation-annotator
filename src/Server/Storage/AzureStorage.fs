044e27a3a46ad14896a2f3fddf8b8a1f2e063740
Evelina Gabasova <evelina@evelinag.com>
2019-03-01 11:12:57 +0000

    fixing expert annotations

--- src/Server/Storage/AzureStorage.fs
+++ src/Server/Storage/AzureStorage.fs
@@ -189,17 +189,20 @@ let selectConflictingArticle connectionString userName =
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
     let command = "
-WITH conflicts AS (
+WITH annotated_by_user AS (
+    SELECT article_url FROM [annotations]
+    WHERE user_id = @UserId
+),
+conflicts AS (
     SELECT DISTINCT article_url
     FROM [annotations]
     WHERE article_url IN (
         SELECT article_url FROM [annotations]
-        WHERE user_id <> @UserId AND num_sources IS NOT NULL
+        WHERE article_url NOT IN (SELECT * FROM annotated_by_user) AND num_sources IS NOT NULL
         GROUP BY article_url
         HAVING 
             COUNT(*) = 2 AND -- there are two annotations
             MAX(num_sources) - MIN(num_sources) > @Threshold -- difference between them is larger than threshold
-            
     ) 
 )
 SELECT TOP(1) articles_v5.article_url, title, site_name, plain_content 
