d58a21f20a921be7f644307ea3af76bc84c4f062
Evelina Gabasova <evelina@evelinag.com>
2019-03-03 22:18:42 +0100

    Reverting some of the changes

--- src/Server/Storage/AzureStorage.fs
+++ src/Server/Storage/AzureStorage.fs
@@ -167,7 +167,14 @@ let loadArticlesFromFile connectionString = task {
 let selectUnfinishedArticles connectionString userName =
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = System.IO.File.ReadAllText("sql/select_unfinished_articles.sql")
+    let command = "
+WITH unfinished_articles AS (
+    SELECT article_url 
+    FROM [annotations] 
+    WHERE user_id =@UserId AND annotation IS NULL
+)
+SELECT articles_v5.article_url, title, site_name, plain_content FROM [articles_v5] 
+INNER JOIN unfinished_articles ON articles_v5.article_url = unfinished_articles.article_url"
     use cmd = new SqlCommand(command, conn)
     cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
 
@@ -181,7 +188,26 @@ let selectConflictingArticle connectionString userName =
     // Select an article that has two annotations with conflicting number of sources identified
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = System.IO.File.ReadAllText("sql/select_conflicting_articles.sql") 
+    let command = "
+WITH annotated_by_user AS (
+    SELECT article_url FROM [annotations]
+    WHERE user_id = @UserId
+),
+conflicts AS (
+    SELECT DISTINCT article_url
+    FROM [annotations]
+    WHERE article_url IN (
+        SELECT article_url FROM [annotations]
+        WHERE article_url NOT IN (SELECT * FROM annotated_by_user) AND num_sources IS NOT NULL
+        GROUP BY article_url
+        HAVING 
+            COUNT(*) = 2 AND -- there are two annotations
+            MAX(num_sources) - MIN(num_sources) > @Threshold -- difference between them is larger than threshold
+    ) 
+)
+SELECT TOP(1) articles_v5.article_url, title, site_name, plain_content 
+FROM [articles_v5] INNER JOIN conflicts 
+ON articles_v5.article_url = conflicts.article_url"  
 
     use cmd = new SqlCommand(command, conn)
     cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
@@ -196,7 +222,21 @@ let selectNextTrainingArticle connectionString userName =
     // Select article in the training batch that was not annotated by the current user
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = System.IO.File.ReadAllText("sql/select_next_training_article.sql")
+    let command = "
+WITH user_annotations AS (
+    SELECT article_url, annotation FROM [annotations]
+    WHERE user_id = @UserId
+), 
+training_articles AS (
+    SELECT batch_article_test.article_url 
+    FROM [batch_article_test] 
+        LEFT JOIN user_annotations
+        ON batch_article_test.article_url = user_annotations.article_url
+    WHERE minibatch_id = 0 AND annotation IS NULL
+)
+SELECT TOP(1) articles_v5.article_url, title, site_name, plain_content 
+FROM [articles_v5] 
+WHERE articles_v5.article_url IN (SELECT article_url FROM training_articles)"  
 
     use cmd = new SqlCommand(command, conn)
     cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
@@ -210,7 +250,15 @@ let selectFinishedArticles userName connectionString maxCount =
     // Select all articles annotated by the current user previously
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = System.IO.File.ReadAllText("sql/select_finished_articles.sql")  
+    let command = "
+WITH annotated AS (
+    SELECT TOP(@Count) article_url FROM [annotations]
+    WHERE user_id = @UserId AND num_sources IS NOT NULL
+    ORDER BY updated_date DESC
+)
+SELECT articles_v5.article_url, title, site_name, plain_content 
+FROM [articles_v5] INNER JOIN annotated 
+ON articles_v5.article_url = annotated.article_url"  
 
     use cmd = new SqlCommand(command, conn)
     cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
@@ -226,8 +274,69 @@ let selectNextStandardArticle connectionString userName =
     printfn "Choosing new article to annotate"
     use conn = new System.Data.SqlClient.SqlConnection(connectionString.SqlConnection)
     conn.Open()
-    let command = System.IO.File.ReadAllText("sql/select_next_article.sql")
+    let command = "
+DECLARE @selected_url NVARCHAR(800);
 
+-- all articles annotated by user
+WITH user_annotations AS ( 
+    SELECT article_url, created_date
+    FROM [annotations]
+    WHERE user_id = @UserId   
+),
+-- all articles that need annotations, regardless of user
+all_articles_that_need_annotation AS (   
+    SELECT minibatch_id, batch_article_test.article_url
+    FROM [batch_article_test] 
+        LEFT JOIN annotations ON batch_article_test.article_url = annotations.article_url
+    GROUP BY batch_article_test.article_url, batch_article_test.minibatch_id
+    HAVING COUNT(*) < 2 AND minibatch_id > 0
+),
+-- all articles that need annotation, excluding articles already annotated by the current user
+batch_article_to_annotate AS (
+    SELECT * 
+    FROM all_articles_that_need_annotation 
+    WHERE article_url NOT IN (SELECT article_url FROM user_annotations)
+),
+-- count proportion of user-annotated articles in the batches that require adding an annotation
+annotated_in_minibatch AS ( 
+    SELECT minibatch_id, CAST(COUNT(batch_article_to_annotate.article_url) AS FLOAT) AS n_total, CAST(COUNT(user_annotations.created_date) AS FLOAT) AS n_annotated
+    FROM batch_article_to_annotate 
+        LEFT JOIN user_annotations
+        ON user_annotations.article_url = batch_article_to_annotate.article_url
+    GROUP BY minibatch_id
+),
+-- take the first minibatch that needs annotations added from the current user
+selected_minibatch AS ( 
+    SELECT TOP(1) annotated_in_minibatch.minibatch_id, n_annotated/n_total AS proportion
+    FROM annotated_in_minibatch 
+        INNER JOIN batch_info_test ON annotated_in_minibatch.minibatch_id = batch_info_test.minibatch_id
+    WHERE n_annotated/n_total < @AnnotatedProportion AND priority > 0
+    ORDER BY priority
+),
+-- articles in the selected minibatch
+potential_articles AS (
+    SELECT article_url 
+    FROM [batch_article_test]
+    WHERE minibatch_id IN (SELECT minibatch_id FROM selected_minibatch)
+),
+annotated_counts AS (  -- Count how many annotations are there for each article, ignoring articles annotated by the user
+    SELECT potential_articles.article_url, COUNT(created_date) AS n 
+    FROM potential_articles LEFT JOIN [annotations] ON annotations.article_url = potential_articles.article_url
+    WHERE potential_articles.article_url NOT IN (SELECT article_url FROM [annotations] WHERE user_id = @UserId)
+    GROUP BY potential_articles.article_url
+    HAVING COUNT(*) < 2 
+)
+-- sort articles randomly, starting with articles with no annotations
+SELECT @selected_url = (
+    SELECT TOP(100) article_url, n 
+    FROM annotated_counts
+    GROUP BY article_url, n
+    ORDER BY n, NEWID()  
+);
+SELECT articles_v5.article_url, title, site_name, plain_content 
+FROM [articles_v5] 
+WHERE articles_v5.article_url = @selected_url
+"  
     // TODO: test properly
     use cmd = new SqlCommand(command, conn)
     cmd.Parameters.AddWithValue("@UserId", userName) |> ignore
--- src/Server/sql/select_finished_articles.sql
+++
@@ -1,8 +0,0 @@
-WITH annotated AS (
-    SELECT TOP(@Count) article_url FROM [annotations]
-    WHERE user_id = @UserId AND num_sources IS NOT NULL
-    ORDER BY updated_date DESC
-)
-SELECT articles_v5.article_url, title, site_name, plain_content 
-FROM [articles_v5] INNER JOIN annotated 
-ON articles_v5.article_url = annotated.article_url
